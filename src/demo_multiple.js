ca756126fcfb94098949b9e9fd1bd4db891dc017
Sebastian Germesin <neogermi@gmail.com>
2017-11-16 11:03:34 +0100

    added demo for multiple loading bars on same page

---
+++ src/demo_multiple.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import LoadingBar from 'react-redux-loading-bar'
+
+import { fetchPhotos } from './reducers/photos'
+import { calculatePi } from './reducers/pi'
+
+import Header from './components/header'
+import Pi from './components/pi'
+
+class Demo extends React.Component {
+
+  render() {
+    return (
+      <div className="center">
+        <Header />
+        <main className="p3 mx-auto clearfix">
+          <div className="col col-6 relative">
+            <LoadingBar scope="first" />
+            <div className="p2">
+              <Pi
+                pi={this.props.pi.first || 0}
+                handleCalculatePi={() => this.props.actions.calculatePi('first')}
+              />
+            </div>
+          </div>
+          <div className="col col-6 relative">
+            <LoadingBar scope="second" />
+            <div className="p2">
+              <Pi
+                pi={this.props.pi.second || 0}
+                handleCalculatePi={() => this.props.actions.calculatePi('second')}
+              />
+            </div>
+          </div>
+        </main>
+      </div>
+    )
+  }
+}
+
+Demo.propTypes = {
+  actions: PropTypes.object,
+  pi: PropTypes.object,
+}
+
+const mapStateToProps = state => ({
+  pi: state.pi,
+})
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(
+    { fetchPhotos, calculatePi },
+    dispatch,
+  ),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Demo)
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 
 import Demo from './demo'
 import DemoAjax from './demo_ajax'
+import DemoMultiple from './demo_multiple'
 import store from './store'
 
 import './assets/index.css'
@@ -11,6 +12,8 @@ import './assets/index.css'
 let demoComponent
 if (window.location.href.includes('ajax')) {
   demoComponent = <DemoAjax />
+} else if (window.location.href.includes('multiple')) {
+  demoComponent = <DemoMultiple />
 } else {
   demoComponent = <Demo />
 }
