d4063b36e062917f84a69beaac7809d98cf9b231
Matthew Peveler <matt@popsql.com>
2023-12-27 09:31:35 -0500

    Re-export constants from reducer
    Signed-off-by: Matthew Peveler <matt@popsql.com>

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 import LoadingBarContainer, { LoadingBar } from './loading_bar'
 import loadingBarMiddleware from './loading_bar_middleware'
 import {
+  DEFAULT_SCOPE,
+  HIDE,
   hideLoading,
   loadingBarReducer,
+  RESET,
   resetLoading,
+  SHOW,
   showLoading,
 } from './loading_bar_ducks'
 import ImmutableLoadingBar from './immutable'
 
 export {
+  DEFAULT_SCOPE,
+  HIDE,
   hideLoading,
   ImmutableLoadingBar,
   LoadingBar,
   loadingBarMiddleware,
   loadingBarReducer,
+  RESET,
   resetLoading,
+  SHOW,
   showLoading,
 }
 export default LoadingBarContainer
